041479665d8880a722cbd20f97cd8572936cf41a
artAva <artAva@AvA_PC>
2014-01-21 16:21:08 +0400

    я только что добавил одну переменную ))

--- src/art/Ava/artporject/MainActivity.java
+++ src/art/Ava/artporject/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	// 
+	int some_peremenn1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
